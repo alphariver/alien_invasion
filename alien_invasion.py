ce7ba0a96107ed45709fc25211032f133aea54f3
nkjucc <alphabity>
2024-08-12 17:03:25 +0800

    2024-08-12 add ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,27 +1,51 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.color = (230, 230, 230)
-
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         #设置窗口标题
         pygame.display.set_caption("Alien Invasion litingzhang")
 
+        self.ship = Ship(self)
+
+    def _check_event(self):
+        """响应键盘和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
+
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.color)
-
-            #更新窗口
-            pygame.display.flip()
+            self._check_event()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
 
 
---
+++ ship.py
@@ -0,0 +1,39 @@
+import pygame
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.settings = ai_game.settings
+
+        #加载飞船图像并获取矩形
+        self.image = pygame.image.load('images/ship2.bmp')
+        self.rect = self.image.get_rect()
+
+        #使飞船放在屏幕底部中央
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        #在飞船的属性中存储一个浮点数
+        self.x = float(self.rect.x)
+
+        #移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #根据self.x更新rect对象
+        self.rect.x = self.x
+
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+        
+
+    
